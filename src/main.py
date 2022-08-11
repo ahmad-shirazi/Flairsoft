848ed0b7141946a122e6582acca33a118a0a26fa
amin.karimi <amin.karimi1992@gmail.com>
2022-08-11 13:18:53 -0400

    add minio and database - v1

--- src/main.py
+++ src/main.py
@@ -1,8 +1,21 @@
-from src.application import uploader, data_gather, text_fetcher
+# from src.application import uploader, data_gather, text_fetcher
+#
+#
+# def main():
+#     while True:
+#         data_gather.run()
+#         uploader.run()
+#         text_fetcher.run()
 
 
-def main():
-    while True:
-        data_gather.run()
-        uploader.run()
-        text_fetcher.run()
+import SimpleHTTPServer
+import SocketServer
+
+PORT = 8001
+
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+print("serving at port ", PORT)
+httpd.serve_forever()
