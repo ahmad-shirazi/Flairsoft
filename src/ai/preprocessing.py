2b697c5b8df330aca7833c3a3d5d1c75b2dfc9ef
ahmadmhirazi <105120174+ahmadmhirazi@users.noreply.github.com>
2022-08-14 20:30:13 -0400

    Update preprocessing.py

--- src/ai/preprocessing.py
+++ src/ai/preprocessing.py
@@ -13,26 +13,27 @@ import matplotlib.colors
 from sklearn.cluster import spectral_clustering
 from sklearn.feature_extraction import image
 import cv2
-from google.colab.patches import cv2_imshow
 
-imga = cv2.imread('image001.png', 0)
+def denoise(image):
+    imga = cv2.imread(image, 0)
 
-img = 255 - np.uint8(imga)
-kernel = np.ones((3, 3), np.uint8)
-  
-# Using cv2.erode() method 
-image1 = cv2.erode(img, kernel) 
-image = cv2.erode(image1, kernel) 
-# image = cv2.dilate(img, kernel) 
-  
-# Displaying the image 
-data = 255 - np.uint8(image)
-kernel = np.array([[-1, -1, -1],
-                   [-1, 9,-1],
-                   [-1, -1, -1]])
-image_sharp = cv2.filter2D(src=data, ddepth=-1, kernel=kernel)
-cv2_imshow(data)
-cv2_imshow(255-image1)
-cv2_imshow(image_sharp)
-cv2_imshow(imga)
+    img = 255 - np.uint8(imga)
+    kernel = np.ones((3, 3), np.uint8)
+    
+    # Using cv2.erode() method 
+    image1 = cv2.erode(img, kernel) 
+    image = cv2.erode(image1, kernel) 
+    # image = cv2.dilate(img, kernel) 
+    
+    # Displaying the image 
+    data = 255 - np.uint8(image)
+    kernel = np.array([[-1, -1, -1],
+                    [-1, 9,-1],
+                    [-1, -1, -1]])
+    image_sharp = cv2.filter2D(src=data, ddepth=-1, kernel=kernel)
+    # cv2.imshow(data)
+    # cv2.imshow(255-image1)
+    dnimage = cv2.imwrite('dnimage.png', img)
+    return dnimage
+
 
