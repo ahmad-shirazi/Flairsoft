861a629734542509d0649c80d5e399b196609c50
amin.karimi <amin.karimi1992@gmail.com>
2022-08-20 12:41:39 -0400

    add get result and get status

---
+++ src/gateway.py
@@ -0,0 +1,44 @@
+from config import web_server as server_config
+
+from handler.api import search, post_data, get_status
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import cgi
+from urllib.parse import urlparse, parse_qs
+import json
+
+
+class HandleRequests(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_headers()
+        parsed_url = urlparse(self.path)
+
+        parsed = parse_qs(parsed_url.query)
+        print(parsed_url)
+        result = ""
+        if self.path.__contains__('search'):
+            result = search(parsed["key"])
+
+        if self.path.__contains__('get_status'):
+            result = get_status(parsed["key"])
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers()
+        self.wfile.write(bytes(json.dumps(result), "utf8"))
+
+    def do_POST(self):
+        '''Reads post request body'''
+        self._set_headers()
+        content_len = int(self.headers.getheader('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        self.wfile.write("received post request:<br>{}".format(post_body))
+
+    def do_PUT(self):
+        self.do_POST()
+
+
+HTTPServer((server_config.HOST, server_config.PORT), HandleRequests).serve_forever()
--- src/handler/api.py
+++ src/handler/api.py
@@ -1,14 +1,42 @@
-result = dict()
+from src.dataaccess.document import fetch_by_file_key
+
+import asyncio
+
+
+async def _search(keys):
+    result = dict()
+    docs = await fetch_by_file_key(keys[0])
+    result["req_status"] = "ok"
+    if len(docs) <= 0:
+        result["req_status"] = "bad_req"
+    else:
+        doc = docs[0]
+        result["result"] = doc.result
+    return result
 
 
 def search(key):
-    result["key"] = "search"
+    event_loop = asyncio.get_event_loop()
+    res = event_loop.run_until_complete(_search(key))
+    return res
+
+
+async def _get_status(keys):
+    result = dict()
+    docs = await fetch_by_file_key(keys[0])
+    result["req_status"] = "ok"
+    if len(docs) <= 0:
+        result["req_status"] = "bad_req"
+    else:
+        doc = docs[0]
+        result["status"] = doc.status
     return result
 
 
 def get_status(key):
-    result["key"] = "get_status"
-    return result
+    event_loop = asyncio.get_event_loop()
+    res = event_loop.run_until_complete(_get_status(key))
+    return res
 
 
 def post_data(key):
